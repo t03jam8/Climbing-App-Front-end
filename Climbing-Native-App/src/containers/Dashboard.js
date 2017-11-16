3e99f90ad8b8820f6d285819869defbb7391e226
Kriszti Tóth <toth.kriszti@yahoo.com>
2017-11-16 15:51:54 +0100

    style: dashboard nothing loading screen is fixed, App component background image settings fixed

--- Climbing-Native-App/src/containers/Dashboard.js
+++ Climbing-Native-App/src/containers/Dashboard.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, Image, StyleSheet, View } from 'react-native';
+
 
 class Dashboard extends Component {
-  static propTypes = {
-    content: React.PropTypes.string.isRequired,
-    textStyles: React.PropTypes.oneOfType([
-      React.PropTypes.array,
-      React.PropTypes.number,
-      React.PropTypes.shape({}),
-    ]).isRequired,
-    buttonStyles: React.PropTypes.oneOfType([
-      React.PropTypes.array,
-      React.PropTypes.number,
-      React.PropTypes.shape({}),
-    ]).isRequired,
-  }
-  render = () => {
-    const { textStyles, buttonStyles, content } = this.props;
+
+  render () {
+    const { content } = this.props;
 
     return (
-      <TouchableOpacity style={buttonStyles}>
-        <Text style={textStyles}>{content}</Text>
-      </TouchableOpacity>
+      <View style={styles.dash}>
+        <Image style={styles.dashimage} source={{uri: 'https://cdn.ukc2.com/i/259558.jpg'}}/>
+        <Text style={styles.dashboardtext}>Route setting in process...</Text>
+      </View>
     );
   }
 }
+
+//needs business logic, and if that is in place, we will have to implement the if-else statement here and render
+// WallListComponent
+
+const styles = StyleSheet.create({
+  dash: {
+    backgroundColor: 'transparent',
+  },
+  dashimage: {
+    width: 300,
+    height: 200,
+    overflow: 'visible',
+    backgroundColor: 'transparent',
+    marginTop: 40,
+    marginLeft: 40,
+    marginRight: 40
+  },
+  dashboardtext: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 25,
+    overflow: 'visible',
+    backgroundColor: 'transparent'
+  }
+});
+
+
 export default Dashboard;
